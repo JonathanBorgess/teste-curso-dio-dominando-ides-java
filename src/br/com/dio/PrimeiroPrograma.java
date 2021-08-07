97bd93dd4d4fe364be1a4d15df4719bd005693ac
Jonathan Borges <borgesjonathan80@gmail.com>
2021-08-07 00:08:44 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,48 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato ();
+		livro livro = new livro();
+		
+		System.out.println(gato); 
+		System.out.println(livro);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + (a+b));*/
+
+	}
+	
+
+class Livros {
+	private String nome;
+	private String npage;
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the npage
+	 */
+	public String getNpage() {
+		return npage;
+	}
+	/**
+	 * @param npage the npage to set
+	 */
+	public void setNpage(String npage) {
+		this.npage = npage;
+	}}
+}
